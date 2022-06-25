228f5c36d875157db88e62fde43205498b361ac3
Florin <flocuconasu10@gmail.com>
2022-06-25 18:12:16 +0200

    Created Complete Graph DB

--- cleansing_movies.py
+++ cleansing_movies.py
@@ -1,11 +1,24 @@
 import pandas as pd
+import numpy as np
 
-# Ratings
-path_ratings = "./datasets/ratings.csv"
-df_ratings = pd.read_csv(path_ratings)
+# # Ratings
+# path_ratings = "./datasets/ratings.csv"
+# df_ratings = pd.read_csv(path_ratings)
 
-# TODO: associate names with ids
+# # Names
+# path_names = "./datasets/names.csv"
+# df_names = pd.read_csv(path_names)
 
+# num_users = len(df_ratings['userId'].unique())
+# idx = pd.Index([i+1 for i in range(num_users)])
+
+# path_ratings_names = "./datasets/ratings_names.csv"
+# df_names['userId'] = idx
+
+# # Join of names on userId
+# # pd.merge(df_ratings, df_names, on="userId").to_csv(path_ratings_names, index=False)
+
+# print(df_)
 
 # Movies
 path_movies = "./datasets/movies.csv"
@@ -14,6 +27,7 @@ df_movies = pd.read_csv(path_movies)
 # Check if all movies have the year in the title
 df_movies['title'] = df_movies['title'].apply(lambda x: x.rstrip())
 no_year = df_movies['title'].apply(lambda x : not x[-5:-1].isdigit())
+# no_year is not empty -> there are movies without year
 
 # Movies without year
 movies_no_year = df_movies['title'][no_year]
@@ -22,10 +36,11 @@ movies_no_year = df_movies['title'][no_year]
 df_movies['year'] = df_movies['title'].apply(lambda x : x[-5:-1] if x[-5:-1].isdigit() else None)
 
 # Remove year from 'title'
-df_movies['title'] = df_movies['title'][~no_year].apply(lambda x: x[:-6].rstrip())
+df_movies['title'] = df_movies['title'].apply(lambda x: x[:-6].rstrip() if x[-5:-1].isdigit() else x)
 
 # print(df_movies.sample(50))
 
 path_movies_clean = "./datasets/cleaned_movies.csv"
 # Create cleaned dataset
 # df_movies.to_csv(path_movies_clean, index=False)
+
--- create_graph_database.py
+++ create_graph_database.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 class Neo4jConnection:
 
-    def __init__(self, uri, user, pwd, db):
+    def __init__(self, uri: str, user: str, pwd: str, db: str = None):
         self.__uri = uri
         self.__user = user
         self.__pwd = pwd
@@ -19,7 +19,7 @@ class Neo4jConnection:
         if self.__driver is not None:
             self.__driver.close()
 
-    def query(self, query, parameters=None):
+    def query(self, query: str, parameters=None) -> list:
         assert self.__driver is not None, "Driver not initialized!"
         session = None
         response = None
@@ -35,8 +35,8 @@ class Neo4jConnection:
         return response
 
 
-def add_movies(movies, batch_size=50):
-    # Adds category nodes to the Neo4j graph.
+def add_movies(movies: pd.DataFrame, batch_size: int = 3000):
+    # Adds movie nodes to the Neo4j graph.
     query = '''
             UNWIND $rows AS row
             MERGE (c:Movie {movieId: row.movieId, title: row.title})
@@ -45,7 +45,53 @@ def add_movies(movies, batch_size=50):
 
     return insert_data(query, movies, batch_size)
 
-def insert_data(query, rows, batch_size = 10000):
+
+def add_genres(genres: pd.DataFrame, batch_size: int = 300):
+    # Adds genres nodes to the Neo4j graph.
+    query = '''
+            UNWIND $rows AS row
+            MERGE (c:Genre {name: row.name})
+            RETURN count(*) as total
+            '''
+                        # $rows is substituted with the following dictionary
+    return insert_data(query, genres, batch_size)
+
+def add_has_genres(movies_with_genres: pd.DataFrame, batch_size: int = 5000):
+    # Adds has_genres relationship to the Neo4j graph.
+    query = ''' 
+            UNWIND $rows AS row
+            MERGE (m:Movie {title : row.title})
+            
+            WITH m, row
+            UNWIND row.genres AS genre
+            MATCH (g:Genre {name : genre})
+            MERGE (m)-[:HAS_GENRE]->(g)
+            RETURN count(*) as total
+            '''
+    return insert_data(query, movies_with_genres, batch_size)
+
+def add_users(users: pd.DataFrame, batch_size: int = 300):
+    # Adds user nodes to the Neo4j graph.
+    query = '''
+            UNWIND $rows AS row
+            MERGE (u:User {userId: row.userId, name: row.name})
+            RETURN count(*) as total
+            '''
+
+    return insert_data(query, users, batch_size)
+
+def add_rel_rated(ratings: pd.DataFrame, batch_size: int = 10000):
+    # Adds user nodes to the Neo4j graph.
+    query = '''
+            UNWIND $rows as row
+            MATCH (u:User {userId: row.userId}), (m:Movie {movieId : row.movieId})
+            MERGE (u)-[r:RATED {rating : row.rating, timestamp : row.timestamp}]->(m)
+            RETURN count(*) as total
+            '''
+
+    return insert_data(query, ratings, batch_size)
+
+def insert_data(query: str, rows: pd.DataFrame, batch_size: int):
     # Function to handle the updating the Neo4j database in batch mode.
     total = 0
     batch = 0
@@ -67,9 +113,9 @@ def insert_data(query, rows, batch_size = 10000):
         
         total += res[0]['total']
         batch += 1
-        result = {"total":total, 
-                  "batches":batch, 
-                  "time":time.time()-start}
+        result = {"total": total, 
+                  "batches": batch, 
+                  "time": time.time()-start}
         print(result)
         
     return result
@@ -77,13 +123,38 @@ def insert_data(query, rows, batch_size = 10000):
 
 if __name__ == "__main__":
     conn = Neo4jConnection("bolt://localhost:7687", "neo4j", "ciao", "recommendation-movies")
-    # q = conn.query("CREATE (n:Person {name: 'Andy', title: 'Developer'})")
 
     # Retrieve Movies
     path_movies = "./datasets/cleaned_movies.csv"
     df_movies = pd.read_csv(path_movies)
 
-    first_movies = df_movies[:100][['movieId', 'title']]
-    add_movies(first_movies)
+    df_movies['genres'] = df_movies['genres'].apply(lambda x: x.split("|"))
+
+    # Create nodes for movies
+    # add_movies(df_movies[['movieId', 'title', 'year']], batch_size=3000)
+
+    # Retrieve Genres
+    path_genres = "./datasets/genres.csv"
+    df_genres = pd.read_csv(path_genres)
+
+    # Create nodes for genres
+    # add_genres(df_genres)
+
+    # Create relationship between Movie and Genre
+    # add_has_genres(df_movies[['title', 'genres']], batch_size=5000)
+
+    # Retrieve Users
+    path_users = './datasets/users.csv'
+    df_users = pd.read_csv(path_users)
+
+    # Create nodes for users
+    # add_users(df_users)
+
+    # Retrieve Ratings
+    path_ratings = './datasets/ratings.csv'
+    df_ratings = pd.read_csv(path_ratings)
+
+    # Create users and relationships with movies through ratings
+    add_rel_rated(df_ratings, 10000)
 
     conn.close()
